11031d6896bec02040b2f7810b1295836fd5ff1e
Kevin Bash <100128505@lubbockisd.net>
2019-09-04 15:47:42 -0500

    Initial commit

---
+++ tuertel v2.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.shape("turtle")
+helk = 1
+helk2 = 1
+turtle.speed(helk)
+turtle.forward(100)
+helk2 += 1
+helk += 1
+turtle.right(90)
+turtle.speed(helk)
+turtle.forward(100)
+helk += 2
+
+
+
+
+turtle.exitonclick()

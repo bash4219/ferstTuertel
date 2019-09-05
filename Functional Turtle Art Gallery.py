b47d1829aa56697dff380bc0b421b8e52a19358d
100128505 <kevin.bash18@gmail.com>
2019-09-05 15:58:15 -0500

    my stuf

---
+++ Functional Turtle Art Gallery.py
@@ -0,0 +1,15 @@
+import turtle
+turtle.shape("turtle")
+fr = 200
+
+def rectangle():
+        turtle.forward(fr)
+        turtle.right(90)
+        turtle.forward(50)
+        turtle.right(90)
+        turtle.forward(fr)
+
+
+
+
+turtle.exitonclick()
